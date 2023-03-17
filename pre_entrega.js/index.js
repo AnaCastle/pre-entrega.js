58efbce539f29612f18b83fdf5785cf8d76d36dd
Ana Castillo <ana.fc40@gmail.com>
2023-03-17 20:51:13 -0300

    se le agregan variables de distintos bancos de referencia al codigo fuente de la pagina web

--- pre_entrega.js/index.js
+++ pre_entrega.js/index.js
@@ -1,3 +1,35 @@
+//verificar antes de enviar en que lugar corresponde las nuevas variables de banco!! 
+
+const bancos = [
+    new tipoBanco('BNA', 1),
+    new tipoBanco('Brubank', 2),
+    new tipoBanco('Santander', 3),
+    new tipoBanco('Macro', 4),
+  ]
+  
+  let bancoDeseado = parseInt(prompt(
+      'Escoge tu banco: 1.BNA - 2.Brubank - 3.Santander - 4.Macro'))
+  // 1 - 2 - 3 - 4
+  
+  // variable para condicion de banco correcto
+  let escogioBanco = false
+  
+  //variable que contiene informacion del banco deseado
+  let banco
+  while (escogioBanco === false) {
+    banco = bancos.find((banco) => banco.id === bancoDeseado) // banco - undefined
+    if (!banco)
+      bancoDeseado = parseInt(
+        prompt(
+          'Escoge el numero de tu banco  1.BNA - 2.Brubank - 3.Santander - 4.Macro'
+        )
+      ) 
+
+let banco = parseInt(prompt("Cual es su banco?.")) 
+};
+
+////////////////////HASTA ACA///////////////////////////
+
 let monto = parseInt(prompt("Ingresa el monto a invertir."));
 let meses = parseInt(prompt("Ingresar cantidad de meses a invertir."));
 
@@ -21,6 +53,8 @@ if (meses <= 12 && meses > 0){
     }
 } else if(meses < 1){
     alert('La cantidad de meses indicada es menor a la mínima permitida.')
-} else if(meses > 36){
-    alert('La cantidad de meses indicada es mayor a la náxima permitida.')
+} else if(meses > 36)
+
+      {
+    alert('La cantidad de meses indicada es mayor a la máxima permitida.')
 }
